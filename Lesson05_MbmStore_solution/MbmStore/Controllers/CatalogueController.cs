931bbcee9472def1dd5e5994b0f5e1e97fb2760c
jesarbov <jesarbov@gmail.com>
2021-03-19 10:51:35 +0100

    Lesson05_MbmStore_solution

---
+++ Lesson05_MbmStore_solution/MbmStore/Controllers/CatalogueController.cs
@@ -0,0 +1,34 @@
+﻿using MbmStore.Infrastructure;
+using MbmStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MbmStore.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        // GET: Catalogue
+        public IActionResult Index()
+        {
+            // Books list
+            IList<Book> books = new List<Book>();
+            books = Repository.Products.OfType<Book>().ToList();
+            ViewBag.Books = books;
+
+            // MusicCDs list
+            IList<MusicCD> musicCDs = new List<MusicCD>();
+            musicCDs = Repository.Products.OfType<MusicCD>().ToList();
+            ViewBag.MusicCDs = musicCDs;
+
+
+            // Movies list
+            IList<Movie> movies = new List<Movie>();
+            movies = Repository.Products.OfType<Movie>().ToList();
+            ViewBag.Movies = movies;
+
+            return View();
+            
+        }
+    }
+}
---
+++ Lesson05_MbmStore_solution/MbmStore/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MbmStore.Models {
+
+    public class Product {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+
+        // constructor
+        public Product() { }
+
+        // constructor
+        public Product(string title, decimal price) {
+            Title = title;
+            Price = price;
+        }
+    }
+}
