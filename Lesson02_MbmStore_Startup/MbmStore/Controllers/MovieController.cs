6c21708986a1d4470504940f02744583b5eb84a2
jesarbov <jesarbov@gmail.com>
2021-04-23 16:06:29 +0200

    Lesson 02 - startup

---
+++ Lesson02_MbmStore_Startup/MbmStore/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MbmStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MbmStore.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            // create a new Movie object with instance name jungleBook
+            var jungleBook = new Movie("Jungle Book", 160.50m, "junglebook.jpg");
+
+            // assign a ViewBag property to the new Movie object
+            ViewBag.JungleBook = jungleBook;
+
+            // return the default view
+            return View();
+        }
+    }
+}
